074bdbd1ff9cd8cfdf702642a9356a395c9d8376
CJINBNH <alex_egorov_89@mail.ru>
2022-08-28 20:14:18 +0400

    Выполнил задачу ипользуя рандомное заполнение

--- task_041/Program.cs
+++ task_041/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+int[] array = new int[100];
+Random randomFilling = new Random();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randomFilling.Next(-10, 10);
+    if (array[i]%2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine('[' + string.Join(", ", array ) + ']');
+Console.WriteLine($"Количество четных чисел в массиве равно: {count}");

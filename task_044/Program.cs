c11c6ece072342f9c875cfe700bac0811f76f53a
CJINBNH <alex_egorov_89@mail.ru>
2022-08-28 21:25:33 +0400

    Completed task_044

--- task_044/Program.cs
+++ task_044/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+Console.Clear();
+
+int fibonacci1 = 0;
+int fibonacci2 = 1;
+ 
+Console.WriteLine("Количество элементов ряда Фибоначчи: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ 
+int fibonacciSum = 0;
+int count = 0;
+
+while (count < (n - 2))
+{
+    fibonacciSum = fibonacci1 + fibonacci2;
+    fibonacci1 = fibonacci2;
+    fibonacci2 = fibonacciSum;
+    count++;
+    Console.Write($" {fibonacciSum}");
+}
